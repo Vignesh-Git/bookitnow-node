f04289f674d86c4177f5dfb4a5ad5da03abe751c
vignesh_augtra <vignesh@augtra.com>
2023-11-16 11:51:57 +0530

    updated version

--- routes/ping.js
+++ routes/ping.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res)=>{
-    res.send(`${process.env.APP_NAME} Version 2.0 is running in Port ${process.env.PORT_NAME}`)
+    res.send(`${process.env.APP_NAME} Version 3.0 is running in Port ${process.env.PORT_NAME}`)
 })
 
 module.exports = app
