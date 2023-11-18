fedbffaf58a178326ef60790f6fc86a8be90edbb
vignesh_augtra <vignesh@augtra.com>
2023-11-19 01:22:53 +0530

    updated payloaded size

---
+++ routes/booking.js
@@ -0,0 +1,79 @@
+'use strict'
+
+const express = require("express");
+const app = express();
+const errorhandler = require("../utils/errorHandling")
+const modelHelpers = require("../utils/modelHelpers/bookingModelHelpers")
+const { default: mongoose } = require("mongoose");
+
+// need to implement API schema validation
+
+app.post("/api/booking/add", async (req, res) => {
+    try {
+        res.send(await modelHelpers.create(req.body));
+    } catch (e) {
+        errorhandler(e)
+        res.status(500).send(e);
+    }
+})
+
+app.get("/api/booking/get_all", async(req, res) => {
+    try{
+        res.send(await modelHelpers.fetchAll())
+    } catch(e){
+        errorhandler(e)
+        res.status(500).send(e);
+    }
+})
+
+app.get("/api/booking/get_featured_venues", async(req, res) => {
+    try{
+        res.send(await modelHelpers.fetchAllFeaturedVenues())
+    } catch(e){
+        errorhandler(e)
+        res.status(500).send(e);
+    }
+})
+
+app.delete("/api/booking/:id", async (req, res) => {
+    try {
+        if(req.params.id){
+            res.send(await modelHelpers.deleteDocumentById(req.params.id));
+        } else {
+            res.status(500).send("Venue ID is required")
+        }
+    } catch (e) {
+        errorhandler(e)
+        res.status(500).send(e);
+    }
+})
+
+app.put("/api/booking/:id", async (req, res) => {
+    try {
+        if(req.params.id){
+            res.send(await modelHelpers.updateDocument(req.params.id, req.body));
+        } else {
+            res.status(500).send("Venue ID is required")
+        }
+    } catch (e) {
+        errorhandler(e)
+        res.status(500).send(e);
+    }
+})
+
+app.get("/api/booking/:id", async(req, res) => {
+    try{
+        if(req.params.id){
+            const _id = new mongoose.Types.ObjectId(req.params.id);
+            res.send(await modelHelpers.fetchByFilter({_id}))
+        } else {
+            res.status(500).send("Venue ID is required")
+        }
+    } catch(e){
+        errorhandler(e)
+        res.status(500).send(e);
+    }
+    
+})
+
+module.exports = app
---
+++ utils/modelHelpers/bookingModelHelpers.js
@@ -0,0 +1,39 @@
+const model = require("../../models/booking")
+const { default: mongoose } = require("mongoose");
+
+const helpers = {
+
+    create : async (data) => {
+        const user = new model(data);
+        return await user.save();
+    },
+   
+    fetchAll : async () => {
+        return await model.find()
+    },
+
+   
+    fetchByFilter : async (filterData) => {
+        return await model.findOne(
+            {
+                
+                ...filterData
+            })
+    },
+
+    deleteDocumentById : async (id) => {
+        const _id = new mongoose.Types.ObjectId(id);
+        return await model.deleteOne({
+            _id
+        })
+    },
+
+    updateDocument : async (id, data) => {
+        const _id = new mongoose.Types.ObjectId(id);
+        return await model.updateOne({
+            _id
+        }, data)
+    }
+}
+
+module.exports = helpers
