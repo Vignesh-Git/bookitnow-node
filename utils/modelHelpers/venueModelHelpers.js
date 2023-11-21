0ecb974528072fd0b42fd83dfa6ff5d9466dd2c5
vignesh_augtra <vignesh@augtra.com>
2023-11-21 23:28:25 +0530

    populated in venue get

--- utils/modelHelpers/venueModelHelpers.js
+++ utils/modelHelpers/venueModelHelpers.js
@@ -37,7 +37,7 @@ const helpers = {
         return await model.findOne(
             {
                 ...filterData
-            })
+            }).populate('courts.court_id').exec()
     },
 
     deleteDocumentById: async (id) => {
