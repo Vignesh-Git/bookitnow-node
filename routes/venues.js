e7d04300e9b381082a570380eb531e0fd152d824
vignesh_augtra <vignesh@augtra.com>
2023-12-27 23:34:31 +0530

    bug fixes

--- routes/venues.js
+++ routes/venues.js
@@ -145,8 +145,10 @@ app.post("/api/venue/get_filtered_courts_for_booking", async (req, res) => {
         let result = []
         const courtData = await modelHelpers.frameAvailableTimingsGroupByCourt(req.body.venueId, req.body.date);
         result = courtData.filter(oneCourtData => oneCourtData.courtDetails.sport_id == req.body.sportId)
-
-
+        res.send({
+            a : result,
+            b : courtData
+        })
     } catch(e){
         errorhandler(e)
         res.status(500).send(e);
